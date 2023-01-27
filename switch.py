3f2ef24a12921eb87cd3052508f2fe93645e664c
gume <gume75@gmail.com>
2023-01-27 14:49:18 +0100

    2

--- switch.py
+++ switch.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import timedelta
 import logging
 
@@ -10,7 +11,10 @@ from homeassistant.components.switch import SwitchEntity
 
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_NAME
 
+#from homeassistant.helpers.service import async_call_service
+#from homeassistant.components.climate import async_set_temperature
 
 # Configuration and options
 CONF_CLIMATE = "climate"
@@ -23,27 +27,29 @@ _LOGGER: logging.Logger = logging.getLogger(__package__)
 # Validation of the user's configuration
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_CLIMATE): cv.string,
+    vol.Required(CONF_NAME): cv.string,
     vol.Optional(CONF_OPEN_TEMP, default=0.0): vol.Schema(float),
     vol.Optional(CONF_CLOSE_TEMP, default=40.0): vol.Schema(float),
 })
 
-def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
-) -> None:
-    climate = config.get(CONF_CLIMATE)
+async def async_setup_platform(hass, config, async_add_entities, discovery_info = None) -> None:
+    climate_e = config.get(CONF_CLIMATE)
     open_temp = config.get(CONF_OPEN_TEMP)
     close_temp = config.get(CONF_CLOSE_TEMP)
-    add_entities([TRVSwitch(hass, climate, open_temp, close_temp)])
+    name = config.get(CONF_NAME)
+
+    async_add_entities([TRVSwitch(hass, name, climate_e, open_temp, close_temp)])
 
 class TRVSwitch(SwitchEntity):
 
-    def __init__(self, hass, climate, open_temp, close_temp):
-        """Initialize the Example switch."""
-        self._name = "Kapcs"
+    def __init__(self, hass, name, climate, open_temp, close_temp):
+        self._attr_unique_id = "trvswitch_" + name
+        self._name = name
+        self._climate = climate
+        self._open_temp = open_temp
+        self._close_temp = close_temp
         self._state = False
+        self._hass = hass
 
     @property
     def name(self):
@@ -55,12 +61,18 @@ class TRVSwitch(SwitchEntity):
         """Return true if switch is on."""
         return self._state
 
-    def turn_on(self, **kwargs):
+    async def async_turn_on(self, **kwargs):
         """Turn the switch on."""
+        await self._hass.services.async_call("climate", "turn_on", {"entity_id": self._climate})
+        await self._hass.services.async_call("climate", "set_preset_mode", {"entity_id": self._climate, "preset_mode": "manual"})
+        await self._hass.services.async_call("climate", "set_temperature", {"entity_id": self._climate, "temperature": self._open_temp})
         self._state = True
         self.schedule_update_ha_state()
 
-    def turn_off(self, **kwargs):
+    async def async_turn_off(self, **kwargs):
         """Turn the switch off."""
+        await self._hass.services.async_call("climate", "turn_on", {"entity_id": self._climate})
+        await self._hass.services.async_call("climate", "set_preset_mode", {"entity_id": self._climate, "preset_mode": "manual"})
+        await self._hass.services.async_call("climate", "set_temperature", {"entity_id": self._climate, "temperature": self._close_temp})
         self._state = False
         self.schedule_update_ha_state()
